6c36d06989ff0470be4f389fd7da94c162c95725
couto <tesye30@hotmail.com>
2022-11-12 23:59:16 -0300

    printf

---
+++ c00/ex03/03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    char letra = 'a';
+    int nro = 10;
+    float n = 5.25;
+    printf("%c \n", letra);
+    printf("%d \n",letra);
+    printf("%d \n",nro);
+    printf("%f \n",n);
+    system("pause");
+  return 0;
+}
