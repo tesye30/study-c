843b6c23567302eb41bab525d8493cb0dcc35a0d
couto <tesye30@hotmail.com>
2022-11-14 23:30:45 -0300

    scanf

---
+++ c00/ex04/04.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+  int nro;
+  float f;
+  printf("Digite dois numeros: ");
+  scanf("%d%f", &nro, &f);
+  printf ("Numeros: %d e %f\n", nro, f);
+  system("pause");
+  return 0;
+}
