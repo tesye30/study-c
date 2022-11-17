89003040948e7b9e3de8ffb268cf382a82cb9817
couto <tesye30@hotmail.com>
2022-11-16 23:58:59 -0300

    1005 1006 Media1/2

---
+++ uri/1005.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    float A, B, X;
+    scanf("%f",&A);
+    scanf("%f",&B);
+    X = (A * 3.5 + B * 7.5)/11;
+    printf("MEDIA = %f\n", X);
+ 
+    return 0;
+}
---
+++ uri/1006.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    float A, B, C, X;
+    scanf("%f",&A);
+    scanf("%f",&B);
+    scanf("%f",&C);
+    X = (A * 2 + B * 3 + C * 5)/10;
+    printf("MEDIA = %2.f\n", X);
+ 
+    return 0;
+}
