763d59fc0df3946c2a367f4a8f330a3d22b4d855
couto <tesye30@hotmail.com>
2022-11-17 23:58:50 -0300

    1007 - diferenca exerc

---
+++ uri/1007.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int A, B, C, D, DIF;
+    scanf("%d",&A);
+    scanf("%d",&B);
+    scanf("%d",&C);
+    scanf("%d",&D);
+    DIF = (A * B - C * D);
+    
+    printf("DIFERENCA = %d\n", DIF);
+ 
+    return 0;
+}
