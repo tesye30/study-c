e40da58ec3dd3e0605b1c57f6b00280e306d1a8b
couto <tesye30@hotmail.com>
2022-11-22 23:52:47 -0300

    pre e pos incremento

---
+++ c00/incremento.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+  int x = 10, y = 10;
+  x++;
+  ++y;
+  printf("x = %d \n", x);
+  printf("y = %d \n", y);
+
+  system("pause");
+  return 0;
+}
