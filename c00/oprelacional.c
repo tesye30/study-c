d04124bbe55c8ccfd5c47d68f7b5e5cc004a5872
couto <tesye30@hotmail.com>
2022-11-23 23:59:32 -0300

    operadores relacionais

---
+++ c00/oprelacional.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  //  >  "Maior do que"
+  //  >= "Maior ou igual"
+  //  <  "Menor do que"
+  //  <= "Menor ou igual"
+  //  == "Igual a"
+  //  != "Diferente de"
+  int x =5, y = 3;
+
+  printf("Resultado = %d\n", x == 4);
+  printf("Resultado = %d\n", x-2 != y);
+  printf("Resultado = %d\n", x = 4);
+
+
+    system("pause");
+    return 0;
+}
